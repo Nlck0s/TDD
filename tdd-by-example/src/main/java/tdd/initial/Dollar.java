a32881d1a71ac2923acf77fdc2e23da58a2d8829
Nlck0s <43850926+Nlck0s@users.noreply.github.com>
2020-08-18 21:49:14 +0200

    Francs and Dollars Implementation

--- tdd-by-example/src/main/java/tdd/initial/Dollar.java
+++ tdd-by-example/src/main/java/tdd/initial/Dollar.java
@@ -1,13 +1,15 @@
 package tdd.initial;
 
-public class Dollar {
-    int amount;
+public class Dollar extends Money{
+
 
     public Dollar(int amount) {
         this.amount = amount;
     }
 
-    void times(int multiplier) {
-        amount *= multiplier;
+    Dollar times(int multiplier) {
+        return new Dollar(amount * multiplier);
     }
+
+
 }
