76addeea954004eba24efe58c1f5568c3643fb8f
Nlck0s <43850926+Nlck0s@users.noreply.github.com>
2020-08-18 20:06:20 +0200

    updates

---
+++ tdd-by-example-master/src/test/java/tdd/initial/MoneyTest.java
@@ -0,0 +1,15 @@
+package tdd.initial;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MoneyTest {
+    @Test
+    void testMulti() {
+        Dollar five=new Dollar(5);
+        five.times(2);
+        assertEquals(10,five.amount);
+
+    }
+}
